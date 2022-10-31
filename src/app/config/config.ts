6dbfdd0f1f9599c081e77a14663bae860f7f3445
RecluseJean <jean.guevara.castro@gmail.com>
2022-10-31 12:13:01 -0500

    config

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,10 +1,10 @@
 // INT/RAMA - LOCALHOST 
-export const URL_SERVICIOSBACK = 'http://localhost:3000/api/';
-export const URL_BASICO = 'http://localhost:3000/'
+// export const URL_SERVICIOSBACK = 'http://localhost:3000/api/';
+// export const URL_BASICO = 'http://localhost:3000/'
 
 // CRT - SERVIDOR ONPREM PTCRT1
-// export const URL_SERVICIOSBACK = 'http://192.168.5.144:3000/api/';
-// export const URL_BASICO = 'http://192.168.5.144:3000/';
+ export const URL_SERVICIOSBACK = 'http://192.168.5.144:3000/api/';
+ export const URL_BASICO = 'http://192.168.5.144:3000/';
 
 // PRD - DIGITAL OCEAN
 // export const URL_SERVICIOSBACK = 'http://192.241.202.237:3000/api/';
